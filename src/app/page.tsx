59ea50e56e8a0be69b8ee68a9353530966b5de73
Orange Lee <orangelee30@gmail.com>
2026-05-10 19:03:03 +0800

    Remove Brands component from page
    Removed Brands component from the main page layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
@@ -20,7 +19,6 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Features />
-      <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
       <Testimonials />
