b453d453fa12e962a62808dc6f2c7604878aa40e
orange-lee-tech <orangelee30@gmail.com>
2026-05-10 23:19:05 +0800

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonials";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function Home() {
       <Features />
       <AboutSectionOne />
       <AboutSectionTwo />
+      <Testimonials />
       <Blog />
       <Contact />
     </>
