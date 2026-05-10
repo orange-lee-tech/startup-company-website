33f6f7511aa7985c9370b024be358487f1a0d774
Orange Lee <orangelee30@gmail.com>
2026-05-10 17:27:23 +0800

    Simplify navigation for company website

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -3,82 +3,28 @@ import { Menu } from "@/types/menu";
 const menuData: Menu[] = [
   {
     id: 1,
-    title: "Home",
+    title: "首页",
     path: "/",
     newTab: false,
   },
   {
     id: 2,
-    title: "About",
+    title: "关于九辰",
     path: "/about",
     newTab: false,
   },
   {
-    id: 33,
-    title: "Blog",
+    id: 3,
+    title: "资讯动态",
     path: "/blog",
     newTab: false,
   },
   {
-    id: 3,
-    title: "Support",
+    id: 4,
+    title: "联系我们",
     path: "/contact",
     newTab: false,
   },
-  {
-    id: 4,
-    title: "Pages",
-    newTab: false,
-    submenu: [
-      {
-        id: 41,
-        title: "About Page",
-        path: "/about",
-        newTab: false,
-      },
-      {
-        id: 42,
-        title: "Contact Page",
-        path: "/contact",
-        newTab: false,
-      },
-      {
-        id: 43,
-        title: "Blog Grid Page",
-        path: "/blog",
-        newTab: false,
-      },
-      {
-        id: 44,
-        title: "Blog Sidebar Page",
-        path: "/blog-sidebar",
-        newTab: false,
-      },
-      {
-        id: 45,
-        title: "Blog Details Page",
-        path: "/blog-details",
-        newTab: false,
-      },
-      {
-        id: 46,
-        title: "Sign In Page",
-        path: "/signin",
-        newTab: false,
-      },
-      {
-        id: 47,
-        title: "Sign Up Page",
-        path: "/signup",
-        newTab: false,
-      },
-      {
-        id: 48,
-        title: "Error Page",
-        path: "/error",
-        newTab: false,
-      },
-    ],
-  },
 ];
+
 export default menuData;
