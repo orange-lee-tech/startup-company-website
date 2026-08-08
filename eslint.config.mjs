dd326a3b87f23a40c5fd0fa4e3f4318dd1937552
Orange Lee <orangelee30@gmail.com>
2026-08-08 22:00:56 +0800

    quality: remove anonymous ESLint config export warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
-export default [
+const config = [
   {
     ignores: [".next/**", "out/**", "node_modules/**"],
   },
   ...compat.extends("next/core-web-vitals"),
 ];
+
+export default config;
