df0d2d80c70d673da5c978cd11ba9f95dc585cc0
Orange Lee <orangelee30@gmail.com>
2026-08-08 21:57:32 +0800

    quality: migrate ESLint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  {
+    ignores: [".next/**", "out/**", "node_modules/**"],
+  },
+  ...compat.extends("next/core-web-vitals"),
+];
